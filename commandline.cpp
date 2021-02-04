eaafb41c4c692079d65a2fc57f899c7fd09ff594
Haochen Yu <haochyu@umich.edu>
2021-02-04 13:57:48 -0500

    Add more words to make it clear

--- commandline.cpp
+++ commandline.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 int main (int argc, char **argv){
-    cout<<argc<<endl;
+    cout<<"There are "<<argc<<" elements"<<endl;
+    cout<<"The command line arguments are: "<<endl;
     for(int i=1;i<argc;i++){
         cout<<*(argv+i)<<endl;
     }
