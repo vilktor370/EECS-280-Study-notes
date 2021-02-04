4e1e0baa25a040b0e86c1a505edab9cfb562bdcd
Haochen Yu <haochyu@umich.edu>
2021-02-04 14:02:01 -0500

    Fix formating issues

--- commandline.cpp
+++ commandline.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 int main (int argc, char **argv){
-    cout<<"There are "<<argc<<" elements"<<endl;
-    cout<<"The command line arguments are: "<<endl;
+    cout<<"The number of inputs(include'./a.out'): "<<argc<<endl;
+    cout<<"The number of elements: "<<argc-1<<endl;
+    cout<<"The command line arguments are: ";
     for(int i=1;i<argc;i++){
-        cout<<*(argv+i)<<endl;
+        cout<<*(argv+i)<<"  ";
     }
 }
